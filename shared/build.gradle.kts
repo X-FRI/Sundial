7c831e1c0ed0efe1980764f676e37b62d808dbbf
Somhairle H. Marisol <muqiu-han@outlook.com>
2026-08-11 13:01:57 +0800

    build: drop unused material and material3 dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -21,8 +21,6 @@ kotlin {
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
-                implementation(compose.material)
-                implementation(compose.material3)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinxDatetimeVersion")
